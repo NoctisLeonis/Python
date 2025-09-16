c31cc56da8565772409a880651f1cbb945b556e9
NoctisLeonis <nelevstinov@gmail.com>
2025-09-16 20:19:31 +0300

    Add files via upload

---
+++ PythonApplication.py
@@ -0,0 +1,41 @@
+1.1
+print(30 + 1, 20 - 2, 80 - 1)
+
+1.2
+numbers = [45 + 2, 50 + 2, 100 + 50]
+print(numbers)
+
+1.3
+print(50)
+print(10)
+
+1.4
+print(5)
+print(10)
+print(21)
+
+1.5
+print(1)
+print(2)
+
+1.6
+
+pi = 3.1415926
+rounded_pi = round(pi, 3)  
+print(rounded_pi) 
+
+1.7
+
+e = 2.71828;
+rounded_e = round(e, 1)
+print(rounded_e)
+
+
+
+
+
+
+
+
+
+
