935a7dc021a64543d3acb721a4d3f3bd7cc03a25
NoctisLeonis <nelevstinov@gmail.com>
2025-09-30 18:13:49 +0300

    Add files via upload

---
+++ 2.4.py
@@ -0,0 +1,3 @@
+storona_kvadrata = 13
+perimetr_kvadrata = storona_kvadrata * 4
+print(f"Периметр квадрата со стороной {storona_kvadrata} равен {perimetr_kvadrata}")
---
+++ 2.5.py
@@ -0,0 +1,3 @@
+radius = float(input("Введите радиус окружности: "))
+diameter = 2 * radius
+print(f"Диаметр окружности равен: {diameter}")
