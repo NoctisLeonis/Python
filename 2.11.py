11852f756f0426473618dd5d3e30f55c82f8a9a9
NoctisLeonis <nelevstinov@gmail.com>
2025-10-27 20:44:13 +0300

    Add files via upload

---
+++ 2.11.py
@@ -0,0 +1,4 @@
+massa = float(input("Введите массу тела (в кг): "))
+obem = float(input("Введите объём тела (в м³): "))
+plotnost = massa / obem
+print(f"Плотность материала: {plotnost:} кг/м³")
---
+++ 2.12.py
@@ -0,0 +1,4 @@
+naselenie = int(input("Введите количество жителей: "))
+ploshad = float(input("Введите площадь территории : "))
+plotnost_naseleniya = naselenie / ploshad
+print(f"Плотность населения: {plotnost_naseleniya:} ")
